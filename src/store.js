8b119c7aad7eda943486dc886f4834219d37ce51
DarioLoForti <lofortidario@gmail.com>
2024-01-11 16:17:57 +0100

    adding serach component

--- src/store.js
+++ src/store.js
@@ -3,5 +3,7 @@ import{ reactive } from 'vue';
 export const store = reactive({
     endpoint: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0 ',
     yu_gi_oh_list: [],
+    endpointarchetype: 'https://db.ygoprodeck.com/api/v7/archetypes.php',
+    archetypes: [],
     loading: true,
 })
